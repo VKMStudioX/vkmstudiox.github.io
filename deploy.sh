94f8c1026a97493bb56a46c01d6dfd267f06f06b
VKMStudioX <VKMStudioX@gmail.com>
2022-10-12 01:08:57 +0200

    change deploy to main

--- deploy.sh
+++ deploy.sh
@@ -16,12 +16,12 @@ echo > .nojekyll
 # echo 'www.example.com' > CNAME
 
 # git init
-# git checkout -B master
+# git checkout -B main
 # git add -A
 # git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:vkmstudiox/vkmstudiox.github.io.git master
+git push -f git@github.com:vkmstudiox/vkmstudiox.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
